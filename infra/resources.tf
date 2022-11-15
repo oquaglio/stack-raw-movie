7ae991d77b92a6d92d292add091921971bea7a12
Otto Quaglio <otto.q@gmx.com>
2022-11-15 12:33:53 +0800

    feat(sf): add func to flatten json array

--- infra/resources.tf
+++ infra/resources.tf
@@ -309,3 +309,37 @@ resource "snowflake_table" "movie" {
   }
 
 }
+
+#
+# Transforms this:
+# [
+#   {
+#     "name": "Genre",
+#     "value": "Action"
+#   },
+#   {
+#     "name": "Year",
+#     "value": "1979"
+#   }
+# ]
+#
+# Into this:
+#
+# {
+#  "Genre": "Action",
+#  "Year": "1979"
+# }
+#
+resource "snowflake_function" "get_name_value_funct" {
+  name     = "GET_NAME_VALUE"
+  database = snowflake_schema.sf_schema.database
+  schema   = snowflake_schema.sf_schema.name
+  arguments {
+    name = "JSON_ARRAY"
+    type = "ARRAY"
+  }
+  comment     = "Function to get value from name"
+  return_type = "OBJECT"
+  language    = "javascript"
+  statement   = "return JSON_ARRAY.reduce((o,i) => { o[i.name] = i.value; return o }, {})"
+}
