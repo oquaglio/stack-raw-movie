5849b1c4c2d5ade7c12850dc16ae8c1c69d1ac06
Otto Quaglio <otto.q@gmx.com>
2022-11-21 08:14:00 +0800

    build: add step to output tfplan.json

--- infra/scripts/deploy_dev.ps1
+++ infra/scripts/deploy_dev.ps1
@@ -5,4 +5,5 @@ terraform fmt
 terraform init
 terraform validate
 terraform plan -out tfplan #generate plan for req'd infra changes
+terraform show -json tfplan | jq > tfplan.json #convert tfplan to json file
 terraform apply tfplan
